6075ce438cfcc9a9427f7e48712a7f0daf51ee2b
Jamie Macdonald <jamie.alban@gmail.com>
2014-09-27 17:02:14 -0400

    Fix status message

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.use(serve('./app'));
 
 app.listen(process.env.PORT || 5000);
 
-console.log('listening on port ' + process.env.PORT || 5000);
+console.log('listening on port ' + (process.env.PORT || 5000));
