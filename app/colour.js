199ebdaed2babd90ca52d6dcf51a9dc2110a3213
Jamie Macdonald <jamie.alban@gmail.com>
2014-09-26 21:13:41 -0400

    Add styling, ace editor, code eval (isitsafe?..tbd)

--- app/colour.js
+++ app/colour.js
@@ -75,15 +75,93 @@
     requestAnimationFrame(step(0));
   };
 
+  colourMe.hsl2rgb = function(H, S, L) {
+    /*
+     * H ∈ [0°, 360°)
+     * S ∈ [0, 1]
+     * L ∈ [0, 1]
+     */
+    H = H % 360;
+
+    /* calculate chroma */
+    var C = (1 - Math.abs((2 * L) - 1)) * S;
+
+    /* Find a point (R1, G1, B1) along the bottom three faces of the RGB cube, with the same hue and chroma as our color (using the intermediate value X for the second largest component of this color) */
+    var H_ = H / 60;
+
+    var X = C * (1 - Math.abs((H_ % 2) - 1));
+
+    var R1, G1, B1;
+
+    if (H === undefined || isNaN(H) || H === null) {
+        R1 = G1 = B1 = 0;
+    }
+    else {
+
+        if (H_ >= 0 && H_ < 1) {
+            R1 = C;
+            G1 = X;
+            B1 = 0;
+        }
+        else if (H_ >= 1 && H_ < 2) {
+            R1 = X;
+            G1 = C;
+            B1 = 0;
+        } else if (H_ >= 2 && H_ < 3) {
+            R1 = 0;
+            G1 = C;
+            B1 = X;
+        } else if (H_ >= 3 && H_ < 4) {
+            R1 = 0;
+            G1 = X;
+            B1 = C;
+        } else if (H_ >= 4 && H_ < 5) {
+            R1 = X;
+            G1 = 0;
+            B1 = C;
+        }
+        else if (H_ >= 5 && H_ < 6) {
+            R1 = C;
+            G1 = 0;
+            B1 = X;
+        }
+    }
+
+    /* Find R, G, and B by adding the same amount to each component, to match lightness */
+
+    var m = L - (C / 2);
+
+    var R, G, B;
+
+    /* Normalise to range [0,255] by multiplying 255 */
+    R = (R1 + m) * 255;
+    G = (G1 + m) * 255;
+    B = (B1 + m) * 255;
+
+    R = Math.round(R);
+    G = Math.round(G);
+    B = Math.round(B);
+
+    return [R, G, B];
+  };
+
 })(window.colourMe = window.colourMe || {}, undefined);
 
 (function($, undefined) {
   $(document).ready(function() {
-    // Expect t, x, y, on normalized intervals [0,1]
-    var rgb = function(t, x, y) {
-      return [256*8*t*x*y, 256*8*(t*(1-x)*(1-y)), 0];
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.setOptions({maxLines: 50, minLines: 10})
+    var canvas = colourMe.createCanvasElement(256, 256, 2, 'colourz', document.getElementById("header"));
+    var steps, cellSize, timeStep;
+    var go = function() {
+      eval(editor.getValue());
+      colourMe.animate(steps === undefined ? 50 : steps, rgb, canvas, cellSize === undefined ? 2: cellSize, timeStep === undefined ? 100 : timeStep);
     };
-
-    colourMe.animate(50, rgb, colourMe.createCanvasElement(256, 256, 2, 'colourz'), 2, 50);
+    go();
+    $("#editor-submit").on("click", go);
   });
 })(jQuery);
+
+
